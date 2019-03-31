bff67e7c916c6924479c6547e977c1271e812631
VonXXGhost <582192766@qq.com>
2019-03-31 11:14:07 +0800

    增加触发更新post信息功能

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ import json
 import pickle
 import re
 
+import requests
+
 app = web.Application()
 
 logging.basicConfig(level=logging.INFO,
@@ -18,9 +20,15 @@ logging.basicConfig(level=logging.INFO,
                     filemode='a')
 logger = logging.getLogger(__name__)
 
+API_username = ''
+API_password = ''
+API_token = ''
+API_refresh = ''
+
 API_BASE_URL = 'http://cqhttp:5700/'
 PRE_BOORU_ABOUT_MSG = {}
-BOT_API_URL_TEMPLATE = 'https://sakugabot.pw/api/posts/{0}'
+BOT_API_REFRESH_URL_TEMPLATE = 'https://sakugabot.pw/api/posts/{0}/refresh/'
+BOT_API_URL_TEMPLATE = 'https://sakugabot.pw/api/posts/{0}/'
 AT_ME = '[CQ:at,qq=1262131302]'
 
 HELP_MESSAGE = '使用指南：\n在群里at本账号同时附上sakugabooru的稿件链接，本账号将搜索是否存在微博gif数据， ' \
@@ -80,16 +88,21 @@ async def message_process(message, data, auto_model=False):
         return
     for id in ids:
         try:
-            async with ClientSession() as session:
-                async with session.get(BOT_API_URL_TEMPLATE.format(id)) as response:
+            async with ClientSession(headers={'Authorization': 'Bearer '+API_token}) as session:
+
+                async with session.post(BOT_API_REFRESH_URL_TEMPLATE.format(id)) as response:
+                    if response.status == 403 and API_token:    # token无效
+                        logger.warning("当前token无效")
+                        refresh_token()
+                        await message_process(message, data, auto_model)
+                        return
                     if response.status == 404:
-                        logger.error('404 of ' + id)
+                        logger.info('404 of ' + id)
                         if not auto_model:
                             await send_message('服务器中暂无此条目数据', data)
                     elif response.status == 200:
                         try:
-                            j = await response.json()
-                            await send_info_and_url(id, j, data)
+                            await send_info_and_url(id, data)
                         except:
                             logger.error(id + ' 暂未有微博数据')
                             if not auto_model:
@@ -102,11 +115,15 @@ async def message_process(message, data, auto_model=False):
             await send_message("服务器出错，请稍后再试", data)
 
 
-async def send_gif_url(gif_url, id, data):
-    await send_message(''.join([id, ':\n', gif_url]), data)
+async def send_info_and_url(id, data):
+    # 使用刷新机制后需重新获取post_info
+    async with ClientSession() as session:
+        async with session.get(BOT_API_URL_TEMPLATE.format(id)) as response:
+            if response.status == 200:
+                post_info = await response.json()
+            else:
+                raise RuntimeError("can't get post info")
 
-
-async def send_info_and_url(id, post_info, data):
     gif_url = post_info['weibo']['img_url']
     if gif_url is None:
         raise RuntimeError('no img url')
@@ -175,6 +192,36 @@ def setup_routes(app):
     app.router.add_post('/', receive)
 
 
+def login():
+    global API_token, API_refresh
+    auth = requests.post('https://sakugabot.pw/api/token/', json={'username': API_username, 'password': API_password})
+    if auth.status_code == 200:
+        logger.info("登录sakugabot成功")
+        API_token = auth.json()['access']
+        API_refresh = auth.json()['refresh']
+    elif 400 <= auth.status_code < 500:
+        logger.error("登录失败，账户或密码错误？")
+        exit(1)
+    else:
+        logger.error("登录失败:\n" + auth.text)
+        exit(1)
+
+
+def refresh_token():
+    global API_token, API_refresh
+    if API_refresh == '':
+        logger.error("未登录，无法刷新token")
+        exit(1)
+    auth = requests.post('https://sakugabot.pw/api/token/refresh/', json={'refresh': API_refresh})
+    if auth.status_code == 200:
+        logger.info("token刷新成功")
+        API_token = auth.json()['access']
+    else:
+        logger.error("token刷新失败:\n" + auth.text)
+        exit(1)
+
+
 if __name__ == '__main__':
+    login()
     setup_routes(app)
     web.run_app(app, host='qqbot', port=5000)
