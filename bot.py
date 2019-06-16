17371e0602a0ee1d2effeaba4535a32f9c100cad
VonXXGhost <582192766@qq.com>
2019-06-16 16:58:59 +0800

    取消post更新，针对新版cqhttp重构

--- bot.py
+++ bot.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from aiohttp import web, ClientSession
-from retrying import retry
 
 import logging
 import os
 import json
 import pickle
 import re
-
-import requests
 
 app = web.Application()
 
@@ -20,15 +17,9 @@ logging.basicConfig(level=logging.INFO,
                     filemode='a')
 logger = logging.getLogger(__name__)
 
-API_username = ''
-API_password = ''
-API_token = ''
-API_refresh = ''
-
 API_BASE_URL = 'http://cqhttp:5700/'
 PRE_BOORU_ABOUT_MSG = {}
-BOT_API_REFRESH_URL_TEMPLATE = 'https://sakugabot.pw/api/posts/{0}/refresh/'
-BOT_API_URL_TEMPLATE = 'https://sakugabot.pw/api/posts/{0}/'
+BOT_API_URL_TEMPLATE = 'https://sakugabot.pw/api/posts/{0}'
 AT_ME = '[CQ:at,qq=1262131302]'
 
 HELP_MESSAGE = '使用指南：\n在群里at本账号同时附上sakugabooru的稿件链接，本账号将搜索是否存在微博gif数据， ' \
@@ -57,73 +48,68 @@ async def receive(request):
     if data['message_type'] != 'group':
         logger.error('非群消息-' + str(data))
         return web.Response()
-    logger.info('组[{0}]人[{1}]：{2}'.format(data['group_id'], data['user_id'], data['message']))
-
     content = data['message']
     group_id = str(data['group_id'])
+    logger.info('组[{0}]人[{1}][{2}]：{3}'.format(group_id, data['user_id'],
+                                               data['sender']['card'] or data['sender']['nickname'], content))
 
+    msg = None
     if 'sakugabooru.com/post/show/' in content:
         PRE_BOORU_ABOUT_MSG[group_id] = content
         if AUTO_SETTINGS.get(group_id, True) and AT_ME not in content:
-            await message_process(content, data, auto_model=True)
+            msg = await message_process(content, auto_model=True)
     if AT_ME in content:
 
         if '-h' in content or '-help' in content:
-            await send_help(data)
+            msg = HELP_MESSAGE
         elif 'sakugabooru.com/post/show/' in content:
-            await message_process(content, data)
+            msg = await message_process(content)
         elif '-no-auto' in content:
-            await auto_setting_process(data, undo=True)
+            msg = await auto_setting_process(data, undo=True)
         elif '-auto' in content:
-            await auto_setting_process(data)
+            msg = await auto_setting_process(data)
         else:
-            await blank_at_process(data)
+            msg = await blank_at_process(group_id)
 
-    return web.Response()
+    if msg is None:
+        return web.Response()
+    else:
+        logger.info('正在发送[{0}]：{1}'.format(group_id, msg))
+        return web.json_response({
+            'at_sender': False,
+            'reply': msg
+        })
 
 
-async def message_process(message, data, auto_model=False):
+async def message_process(message, auto_model=False):
     ids = re.findall(r'(?<=post/show/)(\d+)', message)
     if not ids:
-        return
+        return None
     for id in ids:
         try:
-            async with ClientSession(headers={'Authorization': 'Bearer '+API_token}) as session:
-
-                async with session.post(BOT_API_REFRESH_URL_TEMPLATE.format(id)) as response:
-                    if response.status == 403 and API_token:    # token无效
-                        logger.warning("当前token无效")
-                        refresh_token()
-                        await message_process(message, data, auto_model)
-                        return
+            async with ClientSession() as session:
+                async with session.get(BOT_API_URL_TEMPLATE.format(id)) as response:
                     if response.status == 404:
                         logger.info('404 of ' + id)
                         if not auto_model:
-                            await send_message('服务器中暂无此条目数据', data)
+                            return '服务器中暂无此条目'
                     elif response.status == 200:
                         try:
-                            await send_info_and_url(id, data)
-                        except:
+                            j = await response.json()
+                            return await gene_info_and_url(id, j)
+                        except RuntimeError:
                             logger.error(id + ' 暂未有微博数据')
                             if not auto_model:
-                                await send_message("服务器中暂无此条目数据", data)
+                                return '服务器中暂无此条目微博图片数据'
                     elif not auto_model:
                         logger.error(response.text)
-                        await send_message("服务器出错，请稍后再试", data)
+                        return '服务器出错，请稍后再试'
         except Exception as e:
             logger.error(e)
-            await send_message("服务器出错，请稍后再试", data)
+            return '服务器出错，请稍后再试'
 
 
-async def send_info_and_url(id, data):
-    # 使用刷新机制后需重新获取post_info
-    async with ClientSession() as session:
-        async with session.get(BOT_API_URL_TEMPLATE.format(id)) as response:
-            if response.status == 200:
-                post_info = await response.json()
-            else:
-                raise RuntimeError("can't get post info")
-
+async def gene_info_and_url(id, post_info):
     gif_url = post_info['weibo']['img_url']
     if gif_url is None:
         raise RuntimeError('no img url')
@@ -135,93 +121,38 @@ async def send_info_and_url(id, data):
             copyright.append(tag['main_name'])
         if tag['type'] == 1:
             artist.append(tag['main_name'])
-    text = ''.join([
+    return ''.join([
         str(id), ':\n', '，'.join(copyright), ' ' + source + ' ', '，'.join(artist), '\n' + gif_url
     ])
-    await send_message(text, data)
 
 
-async def send_message(message, data):
-    logger.info('正在发送[{0}]：{1}'.format(data['group_id'], message))
-    await send_group_message(message, data['group_id'])
-    logger.info('已发送[{0}]：{1}'.format(data['group_id'], message))
-
-
-async def send_help(data):
-    await send_message(HELP_MESSAGE, data)
-
-
-@retry(stop_max_attempt_number=3, stop_max_delay=30000)
-async def send_group_message(message, group_id):
-    url = ''.join([API_BASE_URL, 'send_group_msg_async'])
-    params = {
-        'group_id': group_id,
-        'message': message
-    }
-    async with ClientSession() as session:
-        async with session.get(url, params=params) as res:
-            res = await res.json()
-            if res['status'] == 'failed':
-                raise RuntimeError('Send message failed.group[{0}],message[{1}].'.format(group_id, message))
-
-
-async def blank_at_process(data):
-    content = PRE_BOORU_ABOUT_MSG.get(str(data['group_id']), None)
-    logger.info('处理空at from ' + str(data['group_id']))
+async def blank_at_process(group_id):
+    content = PRE_BOORU_ABOUT_MSG.get(group_id, None)
+    logger.info('处理空at from ' + group_id)
     if content:
-        await message_process(content, data)
+        return await message_process(content)
     else:
-        await send_message("没有找到booru网址", data)
+        return '没有找到booru网址'
 
 
-async def auto_setting_process(data, undo=False):
-    name = str(data['group_id'])
+async def auto_setting_process(group_id, undo=False):
     if undo:
-        AUTO_SETTINGS[name] = False
-        await send_message("已取消自动模式", data)
-        logger.info("已取消自动模式-" + name)
+        AUTO_SETTINGS[group_id] = False
+        logger.info('已取消自动模式-' + group_id)
+        msg = '已取消自动模式'
     else:
-        AUTO_SETTINGS[name] = True
-        await send_message("自动检测设置成功", data)
-        logger.info("自动检测设置成功-" + name)
+        AUTO_SETTINGS[group_id] = True
+        logger.info('自动检测设置成功-' + group_id)
+        msg = '自动检测设置成功'
     with open('bot_auto_settings', 'wb') as f:
         pickle.dump(AUTO_SETTINGS, f)
+    return msg
 
 
 def setup_routes(app):
     app.router.add_post('/', receive)
 
 
-def login():
-    global API_token, API_refresh
-    auth = requests.post('https://sakugabot.pw/api/token/', json={'username': API_username, 'password': API_password})
-    if auth.status_code == 200:
-        logger.info("登录sakugabot成功")
-        API_token = auth.json()['access']
-        API_refresh = auth.json()['refresh']
-    elif 400 <= auth.status_code < 500:
-        logger.error("登录失败，账户或密码错误？")
-        exit(1)
-    else:
-        logger.error("登录失败:\n" + auth.text)
-        exit(1)
-
-
-def refresh_token():
-    global API_token, API_refresh
-    if API_refresh == '':
-        logger.error("未登录，无法刷新token")
-        exit(1)
-    auth = requests.post('https://sakugabot.pw/api/token/refresh/', json={'refresh': API_refresh})
-    if auth.status_code == 200:
-        logger.info("token刷新成功")
-        API_token = auth.json()['access']
-    else:
-        logger.error("token刷新失败:\n" + auth.text)
-        exit(1)
-
-
 if __name__ == '__main__':
-    login()
     setup_routes(app)
     web.run_app(app, host='qqbot', port=5000)
